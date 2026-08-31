9e650bed9bedf39ac6aa12f7bca5cd41fb71b2dc
Zach Heindel <33732309+zqhwebpro@users.noreply.github.com>
2026-08-31 11:17:35 -0400

    react-webpack-babel-git

---
+++ new_projects/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function App() {
+    return (
+        <div style={{ textAlign: 'center', marginTop: '50px', fontFamily: 'sans-serif' }}>
+            <h1>Hello World</h1>
+            <p>Welcome to your new React app!</p>
+        </div>
+    );
+}
+
+export default App;
